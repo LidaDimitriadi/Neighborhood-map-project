59b5ee69461cae5e6f943080abd09628dd1e977e
Lida Dimitriadi <lidadimitriadi@hotmail.com>
2016-02-05 12:06:36 +0200

    map loads according to the city the uses picks, still having issues with the css binding and app visibility

--- js/app.js
+++ js/app.js
@@ -18,7 +18,13 @@ var ViewModelMapApp = function() {
 	self.RestaurantsList = ko.observableArray();
 	self.removedRestaurants = [];
 	self.markers = [];
+	self.cityExists = false;
 
+	self.checkEnable = function() {
+		return self.cityExists;
+	}
+
+	console.log(self.cityExists);
 	self.setMapOnAll = function(map) {
 	  for (var i = 0; i < self.markers.length; i++) {
 	    self.markers[i].setMap(map);
@@ -27,75 +33,96 @@ var ViewModelMapApp = function() {
 
 	//Init map, markers and initialRestaurants
 	this.initMap = function() {
-		// Create a map object and specify the DOM element for display.
-		var athens = new google.maps.LatLng(37.9738868, 23.7368454);
-		var map = new google.maps.Map(document.getElementById('map'), {
-		  center: athens,
-		  scrollwheel: false,
-		  zoom: 15
-		});
+		//use the city that the user provides
+		var city = $("#city").val();
+		var APIstr = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=AIzaSyBS025Zl1N-CLVM05-O0_vVO-4heTIpP38';
+		var lat, lng;
 
+		//initialize all arrays and values
 
-		var request = {
-		    location: athens,
-		    radius: '500',
-		    types: ['restaurant']
-		  };
+		self.RestaurantsList([]);
+		self.removedRestaurants = [];
+		self.markers = [];
 
-		// Create the PlaceService and send the request.
-		// Handle the callback with an anonymous function.
-		var service = new google.maps.places.PlacesService(map);
-		service.nearbySearch(request, function(results, status) {
-			// If the request succeeds, draw the place location on
-			// the map as a marker, and register an event to handle a
-			// click on the marker.
-			if (status == google.maps.places.PlacesServiceStatus.OK) {
-				for (var i = 0; i < results.length; i++) {
-				  	var place = results[i];
-				  	//I check if rating for restaurant exists
-				   	if (!place.rating) {
-					    place.rating = "No rating provided";
-				   	}
-			   	   	else {
-				    	place.rating = place.rating.toString();
-				   	}
+		//Ajax request to get coordinates of the requested city and load the map
+		$.getJSON(APIstr).done(function(data) {
+			lat = data.results[0].geometry.location.lat;
+			lng = data.results[0].geometry.location.lng;
+			self.cityExists = true;
+			var loc = new google.maps.LatLng(lat, lng);
+			var map = new google.maps.Map(document.getElementById('map'), {
+			  center: loc,
+			  scrollwheel: false,
+			  zoom: 15
+			});
 
-				   	//create markers and add them to an array for manipulation
-				   	var markers = [];
-				    var marker = new google.maps.Marker({
-					    map: map,
-				     	position: place.geometry.location
-					});
-					self.markers.push(marker);
+			console.log(self.cityExists);
+			var request = {
+			    location: loc,
+			    radius: '500',
+			    types: ['restaurant']
+			  };
 
+			// Create the PlaceService and send the request.
+			// Handle the callback with an anonymous function.
+			var service = new google.maps.places.PlacesService(map);
+			service.nearbySearch(request, function(results, status) {
+				// If the request succeeds, draw the place location on
+				// the map as a marker, and register an event to handle a
+				// click on the marker.
+				if (status == google.maps.places.PlacesServiceStatus.OK) {
+					for (var i = 0; i < results.length; i++) {
+					  	var place = results[i];
+					  	//I check if rating for restaurant exists
+					   	if (!place.rating) {
+						    place.rating = "No rating provided";
+					   	}
+				   	   	else {
+					    	place.rating = place.rating.toString();
+					   	}
 
-
-			   		//create new Restaurant and add it to my list of restaurants
-				    self.RestaurantsList.push( new Restaurant(place.name, place.vicinity, place.rating, map, marker.position) );
-
-				    //display streetview thumbnail when user hovers on marker
-				    (function (i) {
-						google.maps.event.addListener(self.markers[i], 'mouseover', function() {
-							console.log(self.markers[i].position.lng());
-							var contentStr = '<h5><strong>' + self.RestaurantsList()[i].name + '</strong></h5>' + 
-										  '<div id="infoWindowStreetview"><img src="https://maps.googleapis.com/maps/api/streetview?size=200x200&location=' + self.RestaurantsList()[i].marker.lat() + ',' + self.RestaurantsList()[i].marker.lng() + '&heading=151.78&pitch=-0.76&key=AIzaSyBS025Zl1N-CLVM05-O0_vVO-4heTIpP38"></div>';
-								
-										  console.log(contentStr);
-							var infowindow = new google.maps.InfoWindow({
-							    	content: contentStr
-							  	});
-
-							infowindow.open(map, self.markers[i]);
-							//close infoWindow on mouseout
-							google.maps.event.addListener(self.markers[i], 'mouseout', function() {
-								infowindow.close();
-							});
+					   	//create markers and add them to an array for manipulation
+					   	var markers = [];
+					    var marker = new google.maps.Marker({
+						    map: map,
+					     	position: place.geometry.location
 						});
-					})(i);
+						self.markers.push(marker);
 
+
+
+				   		//create new Restaurant and add it to my list of restaurants
+					    self.RestaurantsList.push( new Restaurant(place.name, place.vicinity, place.rating, map, marker.position) );
+
+					    //display streetview thumbnail when user hovers on marker
+					    (function (i) {
+							google.maps.event.addListener(self.markers[i], 'mouseover', function() {
+								console.log(self.markers[i].position.lng());
+								var contentStr = '<h5><strong>' + self.RestaurantsList()[i].name + '</strong></h5>' + 
+											  '<div id="infoWindowStreetview"><img src="https://maps.googleapis.com/maps/api/streetview?size=200x200&location=' + self.RestaurantsList()[i].marker.lat() + ',' + self.RestaurantsList()[i].marker.lng() + '&heading=151.78&pitch=-0.76&key=AIzaSyBS025Zl1N-CLVM05-O0_vVO-4heTIpP38"></div>';
+									
+											  console.log(contentStr);
+								var infowindow = new google.maps.InfoWindow({
+								    	content: contentStr
+								  	});
+
+								infowindow.open(map, self.markers[i]);
+								//close infoWindow on mouseout
+								google.maps.event.addListener(self.markers[i], 'mouseout', function() {
+									infowindow.close();
+								});
+							});
+						})(i);
+
+					}
 				}
-			}
+			});
+			}).error(function() {
+				console.log("error");
+				self.cityExists = false;
 		});
+		console.log(lat, lng);
+		// Create a map object and specify the DOM element for display.
 		
 	};	
 
@@ -138,7 +165,7 @@ var ViewModelMapApp = function() {
 		}
 	}
 	
-	google.maps.event.addDomListener(window, 'load', this.initMap);
+	//google.maps.event.addDomListener(window, 'load', this.initMap);
 
 	//display streetview thumbnail when user hovers on marker
 
@@ -147,12 +174,3 @@ var ViewModelMapApp = function() {
 
 
 ko.applyBindings(new ViewModelMapApp());
-
-var city = "Paris";
-var APIstr = "https://maps.googleapis.com/maps/api/geocode/json?address=Paris&key=AIzaSyBS025Zl1N-CLVM05-O0_vVO-4heTIpP38";
-
-$.getJSON(APIstr, function(data) {
-	console.log(data.results[0].geometry.location.lat);
-}).error(function() {
-	console.log("error");
-});
